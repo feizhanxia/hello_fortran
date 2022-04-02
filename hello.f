c902e661e8458eae3175a7ee9238655d006caecf
feizhanxia <2050570@tongji.edu.cn>
2022-04-03 01:02:33 +0800

    my first fortran file

---
+++ hello.f
@@ -0,0 +1,10 @@
+        PROGRAM hello_fortran
+        INTEGER :: a, b, c
+        WRITE (*,*) 'Input a & b to multiply'
+        READ (*,*) i, j
+        k=i*j
+        WRITE (*,*) 'Result: a * b = ', k
+        STOP
+        END PROGRAM hello_fortran
+
+
